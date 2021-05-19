2e201c0b61d6badfdb6932eb40bb3dff1af035dd
poojan2010 <shahpoojan7828@gmail.com>
2021-05-19 18:28:48 +0530

    updated movie-booking-system

--- resources/views/moviedetails.blade.php
+++ resources/views/moviedetails.blade.php
@@ -9,10 +9,13 @@
     <div class="col-4">
         <img src="{{$movielist->image}}" class="card-img-top rounded"  style="height:550px;">
     </div>
-    <div class="col-4" style="margin-top: 180px;">
+    <div class="col-4">
         <h1>{{ $movielist->title }}</h1>
         <h4>{{ $movielist->release_year }}</h4>
-        <h4>{{ $movielist->cast_member }}</h4>
+        <h4>Cast</h4>
+        {{ $movielist->cast_member }}
+        <h4>About Movie</h4>
+        {{ $movielist->overview }}
     </div>
 
 </div>
