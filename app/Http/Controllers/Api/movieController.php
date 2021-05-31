b14b6c6534acab4539be3536036e318862de0abe
poojan2010 <shahpoojan7828@gmail.com>
2021-05-31 09:25:33 +0530

    movie booking

---
+++ app/Http/Controllers/Api/movieController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\movie;
+
+class movieController extends Controller
+{
+    public function index()
+    {
+        $data = movie::get();
+
+        $response['status'] = 'success';
+        $response['msg'] = 'Show Data';
+        $response['data'] = $data;
+
+        return $response;
+    }
+    public function store(Request $request)
+    {
+        $movie = $request->input();
+        $request->validate([
+            'title' => 'required',
+            'overview' => 'required',
+            'image' => 'required',
+            'release_year' => 'required',
+            'runtime' => 'required',
+            'cast_member' => 'required',
+
+        ]);
+
+        $request->file('image')->move('Uploads',$request->file('image')->getClientOriginalName());
+        $movie = movie::create([
+            'title' => $request->title,
+            'overview' => $request->overview,
+            'image' => $request->file('image')->getClientOriginalName(),
+            'release_year' => $request->release_year,
+            'runtime' => $request->runtime,
+            'cast_member' => $request->cast_member,
+
+        ]);
+
+        $response['status'] = 'success';
+        $response['data'] = $movie;
+
+        return $response;
+    }
+
+    public function update(Request $request, $m_id)
+    {
+        if(is_null($request->file))
+        {
+            movie::where('m_id', $m_id)->update([
+                'title' => $request->title,
+                'overview' => $request->overview,
+                'release_year' => $request->release_year,
+                'runtime' => $request->runtime,
+                'cast_member' => $request->cast_member,
+            ]);
+
+        }
+        else
+        {
+            $request->file('image')->move('Uploads', $request->file('image')->getClientOriginalName());
+            movie::where('m_id', $m_id)->update([
+                'title' => $request->title,
+                'overview' => $request->overview,
+                'image' => $request->file('image')->getClientOriginalName(),
+                'release_year' => $request->release_year,
+                'runtime' => $request->runtime,
+                'cast_member' => $request->cast_member,
+
+            ]);
+
+        }
+
+        $response['status'] = 'success';
+        $response['data'] = [];
+
+        return $response;
+
+    }
+
+    public function destroy($m_id)
+    {
+        $movie = movie::find($m_id);
+        $movie->delete();
+
+        $response['status'] = 'success';
+        $response['data'] = [];
+    }
+
+}
+
+
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\movieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+
+Route::get('/movies',[movieController::class,'index']);
+Route::post('/addmovies',[movieController::class,'store']);
+Route::post('/editmovie/{m_id}', [movieController::class, 'update']);
+Route::get('/deletemovie/{m_id}',[movieController::class,'destroy']);
